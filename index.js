81f436d27ca9f43047bdafd3683e85d7aae7cc30
Mohit Thakkar <thakkarmohit90@gmail.com>
2023-11-21 15:44:48 +0530

    Added files

---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3010;
+
+const webhookClient = new Discord.WebhookClient(
+    {
+        url: process.env.WEBHOOK_URL,
+    }
+);
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome');
+});
+
+app.post('/', (req, res) => {
+    const bodyData = req.body;
+    webhookClient.send(`GM ${bodyData.name}!`);
+    res.send(JSON.stringify(bodyData));
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
+

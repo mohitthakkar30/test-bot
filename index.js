60aaf3e5bf18a3d2a64546903d072597ff502317
Mohit Thakkar <thakkarmohit90@gmail.com>
2023-11-22 14:45:10 +0530

    Major fixes

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
-const Discord = require('discord.js');
-const express = require('express');
-const dotenv = require('dotenv');
+const ethers = require("ethers");
+const Discord = require("discord.js");
+const express = require("express");
+const dotenv = require("dotenv");
 dotenv.config();
+
+const webhookClient = new Discord.WebhookClient({
+  url: process.env.WEBHOOK_URL,
+});
 
 const app = express();
 const port = process.env.PORT || 3010;
 
-const webhookClient = new Discord.WebhookClient(
-    {
-        url: process.env.WEBHOOK_URL,
-    }
-);
-
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Welcome');
+app.get("/", (req, res) => {
+  res.send("Welcome");
 });
 
-app.post('/', (req, res) => {
-    const bodyData = req.body;
-    console.log("bodyData - ", bodyData);
-    webhookClient.send(`Wow, ${bodyData[0].from} just voted...`); 
-    res.send(JSON.stringify(bodyData));
+//Alternate way to send data to discord
+const contractAddress = "0xcFF76d06F90af2668d7a259a2C12C3b88a1A13F3";
+
+const provider = new ethers.providers.JsonRpcProvider(
+  "https://polygon-mumbai.g.alchemy.com/v2/r1xrnkXFSD92Fd715SqGkluuVwQ_5-YK"
+);
+
+const topic0 =
+  "0xcb6783276e8a4347387036bbfea000268f0a4b1f8c46ac79980609f2af8d2acd";
+
+// Create a filter for the specific contract and topic
+const filter = {
+  address: contractAddress,
+  topics: [topic0],
+};
+
+provider.on(filter, (log, event) => {
+  console.log("Event received:");
+  console.log("------------------------");
+  console.log("Address - " + "0x" + log.topics[1].slice(26));
+  webhookClient.send("Wow! " + "0x" + log.topics[1].slice(26) + " just voted!");
 });
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-});
-
-
+console.log("Listening for events...");

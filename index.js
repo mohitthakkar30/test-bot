be914c75b2d35e7702d92303974751f45f2c7e30
Mohit Thakkar <thakkarmohit90@gmail.com>
2023-11-22 14:46:46 +0530

    Major fixes

--- index.js
+++ index.js
@@ -35,9 +35,8 @@ const filter = {
 
 provider.on(filter, (log, event) => {
   console.log("Event received:");
-  console.log("------------------------");
-  console.log("Address - " + "0x" + log.topics[1].slice(26));
   webhookClient.send("Wow! " + "0x" + log.topics[1].slice(26) + " just voted!");
+  console.log("------------------------");
 });
 
 console.log("Listening for events...");
